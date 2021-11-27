999fedfbb650d43f97db2964c2a0d14c072c1f67
ellis <smellis@live.unc.edu>
2021-11-26 22:52:34 -0500

    bleh

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get("/app/user/:id", (req, res) => {
 });
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user/:id", (req, res) => {
-	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?")
+	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?").run(req.body.user, md5(req.body.pass), req.params.id);
 	res.json({"message": "1 record updated: ID " +req.params.id + " (200)"});
 	res.status(200);
 })
