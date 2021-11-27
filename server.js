c8ffe0cc16dac57b70aee2b8ffbe49356fba57b3
ellis <smellis@live.unc.edu>
2021-11-26 22:28:41 -0500

    fixed a stray s

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get("/app/users", (req, res) => {
 });
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
-app.get("/app/users/:id", (req, res) => {	
+app.get("/app/user/:id", (req, res) => {	
 	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = ?").get(req.params.id);
 	res.json(stmt);
 	res.status(200);
